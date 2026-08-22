0817f2f82e950678eb85738fda4010d74e6a88ee
Mendex777 <ilyushkoOo777@yandex.ru>
2026-08-22 19:28:03 +0300

    Align Android JNI bridge with tunnel wrapper

--- android-client/app/src/main/java/ru/metadmin/relay/RelayVpnService.java
+++ android-client/app/src/main/java/ru/metadmin/relay/RelayVpnService.java
@@ -4,6 +4,9 @@ import android.app.*;
 import android.content.*;
 import android.net.VpnService;
 import android.os.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
 import androidx.core.app.NotificationCompat;
 import cc.hev.socks5.tunnel.*;
 
@@ -16,7 +19,9 @@ public class RelayVpnService extends VpnService {
         android.content.SharedPreferences p=getSharedPreferences("relay",MODE_PRIVATE);socks=new PollSocksServer(p.getString("url",""),p.getString("key",""));socks.start();
         Builder b=new Builder().setSession("Metadmin Relay").setMtu(1500).addAddress("10.77.0.2",24).addRoute("0.0.0.0",0).addDnsServer("1.1.1.1");
         b.addDisallowedApplication(getPackageName());tun=b.establish();if(tun==null)throw new Exception("VPN permission missing");
-        hev=new HevSocks5Tunnel();TunnelConfig cfg=new TunnelConfig.Builder().setSocks5Address("127.0.0.1").setSocks5Port(10809).setTunMtu(1500).build();hev.startAsync(cfg,tun.getFileDescriptor());
+        hev=new HevSocks5Tunnel();TunnelConfig cfg=new TunnelConfig.Builder().setSocks5Address("127.0.0.1").setSocks5Port(10809).setTunMtu(1500).build();
+        File configFile=new File(getCacheDir(),"hev.yml");try(FileOutputStream out=new FileOutputStream(configFile)){out.write(cfg.toYaml().getBytes(StandardCharsets.UTF_8));}
+        hev.startAsync(configFile.getAbsolutePath(),tun.getFd());
     }catch(Exception e){e.printStackTrace();stopAll();}}
     private void stopAll(){if(hev!=null){try{hev.stop();}catch(Exception ignored){}hev=null;}if(socks!=null){socks.stop();socks=null;}if(tun!=null){try{tun.close();}catch(Exception ignored){}tun=null;}stopForeground(true);stopSelf();}
     @Override public void onDestroy(){stopAll();super.onDestroy();}
